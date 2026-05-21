94e9001b1d2575b42a2b53235b4964f33976811d
DavidAce <aceituno@kth.se>
2026-05-21 14:08:49 +0200

    Link atomic when available
    - Add the xDMRG++ atomic find module.
    
    - Export atomic as a public dependency when x2tc links it.

--- cmake/SetupDependencies.cmake
+++ cmake/SetupDependencies.cmake
@@ -4,6 +4,13 @@ find_package(OpenMP REQUIRED COMPONENTS CXX)
 target_link_libraries(x2tc-config INTERFACE OpenMP::OpenMP_CXX)
 list(APPEND X2TC_PUBLIC_DEPENDENCIES OpenMP::OpenMP_CXX)
 
+find_package(atomic MODULE BYPASS_PROVIDER)
+if(TARGET atomic::atomic)
+    target_link_libraries(x2tc-config INTERFACE atomic::atomic)
+    list(APPEND X2TC_PUBLIC_DEPENDENCIES atomic::atomic)
+    set(X2TC_HAS_ATOMIC TRUE)
+endif()
+
 find_package(Eigen3 5)
 if(NOT Eigen3_FOUND)
     find_package(Eigen3 3.4 REQUIRED)
